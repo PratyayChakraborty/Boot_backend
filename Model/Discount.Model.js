f7b1359af6aa181c0abbc78bf8764a46e4eab26d
poojakittu <poojayadav.yadav173@gmail.com>
2023-04-29 22:11:10 +0530

    added

--- Model/Discount.Model.js
+++ Model/Discount.Model.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const DiscountSchema = new mongoose.Schema({
   discountName: { type: String, required: true },
   type:[
-    {String, required: true}
-   ]  
+    { type: String, required: true}
+]  
 });
 
 const DiscountModel = mongoose.model("Discount", DiscountSchema);
